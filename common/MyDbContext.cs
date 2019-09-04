4ca30150382a2477a269b8e5329e21c5a865a11a
mengzujun <943885179@qq.com>
2019-09-04 10:29:02 +0800

    添加项目文件。

---
+++ common/MyDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WeixiaoDemo.common
+{
+    public interface IMyDbContext
+    {
+        string GetConnection();
+    }
+    public class MyDbContext : IMyDbContext
+    {
+        public string GetConnection()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ common/SqlHelper.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WeixiaoDemo.common
+{
+    public interface ISqlHelper
+    {
+        string GetSqlConnection();
+    }
+    public class SqlHelper : ISqlHelper
+    {
+        private IConfiguration _configuration;
+        private static IDbConnection connection;
+        public SqlHelper(IConfiguration configuration)
+        {
+            _configuration = configuration;
+            connection = new SqlConnection(_configuration.GetConnectionString("SQLConnection"));
+        }
+
+        public string GetSqlConnection()
+        {
+            return _configuration.GetConnectionString("SQLConnection");
+        }
+    }
+}
